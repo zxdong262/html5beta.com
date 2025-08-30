72e03353234ea892cac763d2e5c803ebb5c9f8f6
ZHAO Xudong <zxdong@gmail.com>
2025-08-30 18:02:11 +0800

    add pug func

--- api/html2pug.js
+++ api/html2pug.js
@@ -1,4 +1,51 @@
+function html2pug (html) {
+  // Remove newlines and extra spaces
+  html = html.replace(/\n/g, '').trim()
 
+  // Match tags like <div class="foo" id="bar">content</div>
+  const tagRegex = /<(\w+)([^>]*)>(.*?)<\/\1>/g
+  let match
+  const pugLines = []
+
+  while ((match = tagRegex.exec(html)) !== null) {
+    const tag = match[1]
+    const attrs = match[2].trim()
+    const content = match[3].trim()
+
+    let pugLine = tag
+
+    // Parse attributes
+    if (attrs) {
+      // Match class and id
+      const classMatch = attrs.match(/class="([^"]+)"/)
+      if (classMatch) {
+        pugLine += '.' + classMatch[1].split(' ').join('.')
+      }
+      const idMatch = attrs.match(/id="([^"]+)"/)
+      if (idMatch) {
+        pugLine += '#' + idMatch[1]
+      }
+      // Other attributes
+      const otherAttrs = []
+      attrs.replace(/(\w+)="([^"]+)"/g, (m, key, value) => {
+        if (key !== 'class' && key !== 'id') {
+          otherAttrs.push(`${key}="${value}"`)
+        }
+      })
+      if (otherAttrs.length) {
+        pugLine += `(${otherAttrs.join(', ')})`
+      }
+    }
+
+    if (content) {
+      pugLine += ` ${content}`
+    }
+
+    pugLines.push(pugLine)
+  }
+
+  return pugLines.join('\n')
+}
 
 export default async function handler (req, res) {
   const {
@@ -10,10 +57,10 @@ export default async function handler (req, res) {
   }
   let pug = ''
   try {
-    pug = ''
+    pug = html2pug(req.body.html)
   } catch (e) {
     console.log(e)
-    return res.status(400).send(e.messgage)
+    return res.status(400).send(e.message)
   }
   res.send({
     pug
