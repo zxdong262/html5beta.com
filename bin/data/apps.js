747e30e2e9b1bde2a805d945130dbaec1536645a
ZHAO Xudong <zxdong@gmail.com>
2026-03-15 13:52:40 +0800

    Add links

--- bin/data/apps.js
+++ bin/data/apps.js
@@ -4,6 +4,16 @@ import resume from './resume.js'
 
 export default [
   resume.openSourced.arr[0],
+  {
+    name: 'China VS rest of the world',
+    description: 'a comparison between China and the rest of the world in terms of GDP per capita, life expectancy, and education.',
+    homepage: 'https://china-vs-rest-of-the-world.html5beta.com'
+  },
+  {
+    name: 'China VS India',
+    description: 'a comparison between China and India in terms of GDP per capita, etc.',
+    homepage: 'https://china-vs-india.html5beta.com'
+  },
   ...readDir(
     resolve(cwd, 'src/views/apps'),
     'apps'
